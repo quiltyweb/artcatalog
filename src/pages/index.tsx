38003a48c600e650b8a943db3dfa13ef02d6aa07
melissa gattoni <meligatt@gmail.com>
2021-07-16 17:37:09 +1000

    enable-typescript-and-linters

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import Layout from '../components/layout';
+
+const IndexPage: React.FunctionComponent = (): React.ReactElement => (
+  <Layout pageTitle="Welcome">
+    <p>~ Work in progress ~</p>
+    <StaticImage alt="acrylic paint texture" src="../images/brushella-texture.jpg" />
+  </Layout>
+);
+
+export default IndexPage;
