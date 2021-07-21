6c237ec5568cc74d61dc192d356a4fd44281f36d
melissa gattoni <meligatt@gmail.com>
2021-07-21 14:12:57 +1000

    add chakra UI library

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
+import { Box, Text } from '@chakra-ui/react';
 import Layout from '../components/layout';
 
 const AboutPage: React.FunctionComponent = (): React.ReactElement => (
   <Layout pageTitle="About Me">
-    <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-
+    <Box p={8}>
+      <Text fontSize="xl">Hi there! I'm the proud creator of this site, which I built with Gatsby.</Text>
+    </Box>
     <StaticImage alt="paint brushes" src="https://brushella.files.wordpress.com/2014/10/dsc04429.jpg" />
   </Layout>
 );
