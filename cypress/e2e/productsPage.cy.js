4d5156b2a00a2b80232ece62d97bdfd01f3a4fce
melissa gattoni <meligatt@gmail.com>
2023-07-19 21:33:10 +1000

    add command to open mobile menu and select and option

--- cypress/e2e/productsPage.cy.js
+++ cypress/e2e/productsPage.cy.js
@@ -15,19 +15,13 @@ describe("Products Page", () => {
     cy.intercept("GET", "/page-data/products/page-data.json", {
       fixture: "products.json",
     });
-    cy.findByRole("button", { name: "menu" }).click();
-    cy.findByText("products").click();
-    cy.findByRole("button", { name: "Close" }).click();
-
+    cy.clickDrawerMenuOption("products");
     cy.findByRole("heading", { name: "Brushella Collections" });
     cy.findByRole("heading", { name: "All Products" });
   });
 
   it("Renders single product page when click on a product item", () => {
-    cy.findByRole("button", { name: "menu" }).click();
-    cy.findByText("products").click();
-    cy.findByRole("button", { name: "Close" }).click();
-
+    cy.clickDrawerMenuOption("products");
     cy.findByRole("heading", { name: "All Products" });
     cy.get("#brushella-all-products-list li a").first().click();
     cy.get("#brushella-single-product-container").within(() => {
@@ -38,10 +32,7 @@ describe("Products Page", () => {
   });
 
   it("Goes back from single product to all products page when click on back button", () => {
-    cy.findByRole("button", { name: "menu" }).click();
-    cy.findByText("products").click();
-    cy.findByRole("button", { name: "Close" }).click();
-
+    cy.clickDrawerMenuOption("products");
     cy.get("#brushella-all-products-list li a").first().click();
     cy.get("#brushella-single-product-container").within(() => {
       cy.findByRole("link", { name: "Back to Product List" }).click();
@@ -50,10 +41,7 @@ describe("Products Page", () => {
   });
 
   it("Goes back from single collection to all products page", () => {
-    cy.findByRole("button", { name: "menu" }).click();
-    cy.findByText("products").click();
-    cy.findByRole("button", { name: "Close" }).click();
-
+    cy.clickDrawerMenuOption("products");
     cy.scrollTo("top");
     cy.get("#brushella-all-collections-list li a").first().click();
     cy.get("#brushella-single-collection-container").within(() => {
@@ -63,24 +51,15 @@ describe("Products Page", () => {
   });
 
   it("Renders single collection page with its products", () => {
-    cy.findByRole("button", { name: "menu" }).click();
-    cy.findByText("products").click();
-    cy.findByRole("button", { name: "Close" }).click();
-
+    cy.clickDrawerMenuOption("products");
     cy.findByRole("heading", { name: "Brushella Collections" });
-
     cy.get("#brushella-all-collections-list li a").first().click();
-
     cy.get("#brushella-all-products-in-collection-list li a").should("exist");
-
     cy.get("#brushella-all-products-in-collection-list li a").first().click();
-
     cy.get("form")
       .findByLabelText(/Quantity/i)
       .should("exist");
-
     cy.findByRole("link", { name: "Back to Product List" }).click();
-
     cy.findByRole("heading", { name: "Brushella Collections" });
   });
 });
