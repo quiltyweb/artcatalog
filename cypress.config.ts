d8b2bedd98b8dadda285ab6abfe99ae16abc36b6
melissa gattoni <meligatt@gmail.com>
2023-07-27 21:10:11 +1000

    updat ecypress e2e to ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,11 @@
+/* eslint-disable no-console */
 import { defineConfig } from "cypress";
-import pluginIndex from "./cypress/plugins/index.js";
 
 export default defineConfig({
   video: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return pluginIndex(on, config);
+      console.log(config);
     },
     baseUrl: "http://localhost:8000/",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
