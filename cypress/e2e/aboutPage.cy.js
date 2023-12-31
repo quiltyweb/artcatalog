10732d0ca46299b6dfe1d91f1a3edadbe098da90
melissa gattoni <meligatt@gmail.com>
2023-12-31 22:32:27 +1100

    update e2e tests with nav menu categories for desktop view

--- cypress/e2e/aboutPage.cy.js
+++ cypress/e2e/aboutPage.cy.js
@@ -13,17 +13,17 @@ describe("About page", () => {
 
   it("loads About page correctly", () => {
     cy.findByRole("button", { name: "menu" }).click();
-    cy.findByRole("link", { name: "About" }).click();
+    cy.findByRole("link", { name: "about" }).click();
     cy.findByText("Meet the Artist");
     // product_categories metaobjects
     cy.findByRole("link", { name: "commissions" });
-    cy.findByRole("link", { name: "original artworks" });
-    cy.findByRole("link", { name: "archival fine art prints" });
+    cy.findByRole("link", { name: "originals" });
+    cy.findByRole("link", { name: "prints" });
     cy.findByRole("link", { name: "resin and pigment art" });
-    cy.findByRole("link", { name: "home and decor" });
+    cy.findByRole("link", { name: "decor" });
     cy.findByRole("link", { name: "wearable art" });
     cy.findByRole("link", { name: "stickers" });
-    cy.findByRole("link", { name: "Murals" });
+    cy.findByRole("link", { name: "murals" });
     cy.findByAltText("Painter Gabriela painting on a canvas");
   });
 });
