a5fb1c537cb6de541b17a6596a80baebc6f98263
melissa gattoni <meligatt@gmail.com>
2024-01-22 13:04:41 +1100

    add favicons

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,4 +1,9 @@
 import * as React from "react";
+import brushellaFavicon1 from "../images/apple-touch-icon.png";
+import brushellaFavicon2 from "../images/favicon-32x32.png";
+import brushellaFavicon3 from "../images/favicon-16x16.png";
+import brushellaFavicon4 from "../images/safari-pinned-tab.svg";
+import brushellaFavicon5 from "../images/favicon.ico";
 
 type SEOProps = {
   children: React.ReactNode;
@@ -10,6 +15,26 @@ const SEO: React.FunctionComponent<SEOProps> = ({
   return (
     <>
       <html lang="en" />
+
+      <link rel="apple-touch-icon" sizes="180x180" href={brushellaFavicon1} />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href={brushellaFavicon2}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href={brushellaFavicon3}
+      />
+      <link rel="manifest" href="../images/site.webmanifest" />
+      <link rel="mask-icon" href={brushellaFavicon4} color="#000000" />
+      <link rel="shortcut icon" href={brushellaFavicon5} />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-config" content="../images/browserconfig.xml" />
+      <meta name="theme-color" content="#ffffff" />
       <link
         rel="stylesheet"
         type="text/css"
