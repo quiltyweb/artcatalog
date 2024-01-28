48714b2e055a0aacd5eab30ad5e15dccfb1cb699
melissa gattoni <meligatt@gmail.com>
2024-01-28 15:43:47 +1100

    update test for product card component

--- src/components/__tests__/ProductCard.spec.tsx
+++ src/components/__tests__/ProductCard.spec.tsx
@@ -49,18 +49,11 @@ describe("ProductCard", () => {
           },
         },
       },
-      collectionHandle: "this is the collectionHandle",
     };
-    render(
-      <ProductCard
-        product={mockedShopifyProductData.product}
-        collectionHandle={mockedShopifyProductData.collectionHandle}
-      />
-    );
+    render(<ProductCard product={mockedShopifyProductData.product} />);
 
     screen.getByText("posavasos title");
     screen.getByText("decor description");
-    screen.getByText(/this is the collectionHandle/);
     screen.getByAltText("This is alt text for testing");
   });
 
@@ -104,17 +97,78 @@ describe("ProductCard", () => {
       },
       collectionHandle: "decor",
     };
-    render(
-      <ProductCard
-        product={mockedShopifyProductData.product}
-        collectionHandle={mockedShopifyProductData.collectionHandle}
-      />
-    );
+    render(<ProductCard product={mockedShopifyProductData.product} />);
 
     screen.getByText("posavasos title");
   });
 
-  it("renders fallback image", async () => {
+  it("renders product description", async () => {
+    const mockedShopifyProductData = {
+      product: {
+        id: "4af45bf5-a1ca-5b57-9318-c7ce027947f0",
+        title: "posavasos title",
+        handle: "posavasos",
+        description: "products description goes here...",
+        priceRangeV2: {
+          maxVariantPrice: {
+            amount: 10,
+            currencyCode: "AUD",
+          },
+        },
+      },
+    };
+
+    render(<ProductCard product={mockedShopifyProductData.product} />);
+
+    screen.getByText("products description goes here...");
+  });
+
+  it("renders product amount in USD dollar ", async () => {
+    const mockedShopifyProductData = {
+      product: {
+        id: "4af45bf5-a1ca-5b57-9318-c7ce027947f0",
+        title: "posavasos title",
+        handle: "posavasos",
+        description: "decor description",
+        priceRangeV2: {
+          maxVariantPrice: {
+            amount: 10,
+            currencyCode: "AUD",
+          },
+        },
+        featuredImage: {
+          altText: "This is alt text for testing",
+          gatsbyImageData: {
+            images: {
+              sources: [
+                {
+                  srcSet:
+                    "https://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_141x115_crop_center.jpg.webp?v=1689332359 141w,\nhttps://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_282x230_crop_center.jpg.webp?v=1689332359 282w,\nhttps://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_564x460_crop_center.jpg.webp?v=1689332359 564w",
+                  sizes: "(min-width: 564px) 564px, 100vw",
+                  type: "image/webp",
+                },
+              ],
+              fallback: {
+                src: "https://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_564x460_crop_center.jpg?v=1689332359",
+                srcSet:
+                  "https://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_141x115_crop_center.jpg?v=1689332359 141w,\nhttps://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_282x230_crop_center.jpg?v=1689332359 282w,\nhttps://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_564x460_crop_center.jpg?v=1689332359 564w",
+                sizes: "(min-width: 564px) 564px, 100vw",
+              },
+            },
+            layout: "constrained",
+            width: 564,
+            height: 460,
+          },
+        },
+      },
+    };
+    render(<ProductCard product={mockedShopifyProductData.product} />);
+
+    screen.getByText(/AUD/i);
+    screen.getByText(/10/i);
+  });
+
+  it("renders fallback image with alt text", async () => {
     const mockedShopifyProductData = {
       product: {
         id: "4af45bf5-a1ca-5b57-9318-c7ce027947f0",
@@ -139,5 +193,50 @@ describe("ProductCard", () => {
     );
 
     screen.getByAltText("no product image available");
+  });
+
+  it("renders form controls", async () => {
+    const mockedShopifyProductData = {
+      product: {
+        id: "4af45bf5-a1ca-5b57-9318-c7ce027947f0",
+        title: "posavasos title",
+        handle: "posavasos",
+        description: "decor description",
+        priceRangeV2: {
+          maxVariantPrice: {
+            amount: 10,
+            currencyCode: "AUD",
+          },
+        },
+        featuredImage: {
+          altText: "This is alt text for testing",
+          gatsbyImageData: {
+            images: {
+              sources: [
+                {
+                  srcSet:
+                    "https://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_141x115_crop_center.jpg.webp?v=1689332359 141w,\nhttps://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_282x230_crop_center.jpg.webp?v=1689332359 282w,\nhttps://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_564x460_crop_center.jpg.webp?v=1689332359 564w",
+                  sizes: "(min-width: 564px) 564px, 100vw",
+                  type: "image/webp",
+                },
+              ],
+              fallback: {
+                src: "https://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_564x460_crop_center.jpg?v=1689332359",
+                srcSet:
+                  "https://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_141x115_crop_center.jpg?v=1689332359 141w,\nhttps://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_282x230_crop_center.jpg?v=1689332359 282w,\nhttps://cdn.shopify.com/s/files/1/0586/9892/4240/files/WhatsAppImage2023-04-28at9.19.54PM_1_564x460_crop_center.jpg?v=1689332359 564w",
+                sizes: "(min-width: 564px) 564px, 100vw",
+              },
+            },
+            layout: "constrained",
+            width: 564,
+            height: 460,
+          },
+        },
+      },
+    };
+    render(<ProductCard product={mockedShopifyProductData.product} />);
+
+    screen.getByLabelText("Quantity");
+    screen.getByRole("button", { name: "Add to cart" });
   });
 });
