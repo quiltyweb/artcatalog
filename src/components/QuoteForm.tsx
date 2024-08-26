80d9cc1075b6e7cb3413749ec879344da874f41c
melissa gattoni <meligatt@gmail.com>
2024-08-26 14:30:49 +1000

    add quote form as a basket backup

---
+++ src/components/QuoteForm.tsx
@@ -0,0 +1,155 @@
+import * as React from "react";
+import {
+  Alert,
+  AlertIcon,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import ShopifyBuy from "shopify-buy";
+
+const SubmitSchema = Yup.object().shape({
+  fullname: Yup.string().max(100, "Too Long!").required("Name is Required"),
+  email: Yup.string().email("Invalid email").required("Email is Required"),
+  message: Yup.string().required("Message is Required"),
+});
+
+interface FormValues {
+  fullname: string;
+  email: string;
+  message: string;
+}
+
+type QuoteFormProps = {
+  checkoutLineItems: Array<ShopifyBuy.CheckoutLineItem>;
+  cartCount: number;
+};
+const QuoteForm: React.FunctionComponent<QuoteFormProps> = ({
+  checkoutLineItems,
+  cartCount,
+}): React.ReactElement => {
+  const getItemsFromBasket = (): string => {
+    const cartForMessage = checkoutLineItems.map((item) => {
+      return `${item.quantity} ${item.title}`;
+    });
+    return cartForMessage.join(" __ ");
+  };
+
+  return (
+    <>
+      {cartCount >= 1 && (
+        <Formik
+          initialValues={{
+            fullname: "",
+            email: "",
+            message: getItemsFromBasket(),
+          }}
+          validationSchema={SubmitSchema}
+          onSubmit={async (
+            { fullname, email, message }: FormValues,
+            { setStatus }
+          ) => {
+            const res = await fetch(
+              "https://www.formbackend.com/f/a89f490517ad6461",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                  Accept: "application/json",
+                },
+                body: JSON.stringify({ fullname, email, message }),
+              }
+            );
+
+            if (!res.ok) {
+              setStatus({
+                sent: false,
+                message:
+                  "There was an error sending your quote. Please try again later.",
+              });
+            } else {
+              if (res.status === 200) {
+                setStatus({
+                  sent: true,
+                  message: "Your quote was sent succesfully!",
+                });
+              }
+            }
+          }}
+        >
+          {(props) => {
+            return (
+              <>
+                {props.status && props.status.sent && (
+                  <Alert status="success" id="bag-status-success">
+                    <AlertIcon />
+                    {props.status.message}
+                  </Alert>
+                )}
+                {props.status && !props.status.sent && (
+                  <Alert status="error">
+                    <AlertIcon />
+                    {props.status.message}
+                  </Alert>
+                )}
+
+                {!props.status && (
+                  <Form data-testid="quote-contact-form">
+                    <Field name="fullname" type="text">
+                      {({ field, form }: any) => (
+                        <FormControl
+                          isInvalid={
+                            form.errors.fullname && form.touched.fullname
+                          }
+                          mb={8}
+                        >
+                          <FormLabel>Full Name</FormLabel>
+                          <Input {...field} />
+                          <FormErrorMessage>
+                            <ErrorMessage name="fullname" />
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+
+                    <Field name="email" type="email">
+                      {({ field, form }: any) => (
+                        <FormControl
+                          isInvalid={form.errors.email && form.touched.email}
+                          mb={8}
+                        >
+                          <FormLabel>Email address</FormLabel>
+                          <Input {...field} />
+                          <FormErrorMessage>
+                            <ErrorMessage name="email" />
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+
+                    <Button
+                      isLoading={props.isSubmitting}
+                      mt={4}
+                      backgroundColor="#86548A"
+                      color="#ffffff"
+                      colorScheme="teal"
+                      type="submit"
+                    >
+                      Get a quote
+                    </Button>
+                  </Form>
+                )}
+              </>
+            );
+          }}
+        </Formik>
+      )}
+    </>
+  );
+};
+
+export default QuoteForm;
