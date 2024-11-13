46ed5b789ea4cfd519920a102d44836fd1c7eb2b
melissa gattoni <meligatt@gmail.com>
2024-11-13 14:04:19 +1100

    update mockup to reflect the changes

--- src/components/__tests__/Layout.spec.tsx
+++ src/components/__tests__/Layout.spec.tsx
@@ -2,100 +2,8 @@ import React from "react";
 import { within } from "@testing-library/dom";
 import { render, screen } from "@testing-library/react";
 import Layout from "../Layout";
-import userEvent from "@testing-library/user-event";
 import * as Gatsby from "gatsby";
-
-const mockUseStaticQuery = {
-  site: {
-    siteMetadata: {
-      title: "Site Title",
-    },
-  },
-  allShopifyCollection: {
-    nodes: [
-      {
-        id: "4179b182-b572-5609-a8a2-05083671479a",
-        title: "Murals & Sign Writing",
-        handle: "murals",
-      },
-      {
-        id: "f824e7dd-2243-5286-a2aa-162cbc0f42cd",
-        title: "Stickers",
-        handle: "stickers",
-      },
-      {
-        id: "a1f141d4-942b-51bc-9ab2-a2f4a48d1755",
-        title: "Wearable Art",
-        handle: "wearable-art",
-      },
-      {
-        id: "1f50e1f9-d3a0-54cc-b961-652870d93340",
-        title: "Resin & Pigment Art",
-        handle: "resin-and-pigment-art",
-      },
-      {
-        id: "62470692-0598-57cc-bc0a-1ea60791c995",
-        title: "Commissions",
-        handle: "commissions",
-      },
-      {
-        id: "bd4e6e5b-a663-5e00-becb-b4a63d4c7ec6",
-        title: "Prints",
-        handle: "prints",
-      },
-      {
-        id: "ab94a31f-8fc3-5e3c-b0cf-5a16c873d647",
-        title: "Original Paintings",
-        handle: "originals",
-      },
-      {
-        id: "384d31c2-36b3-5bc7-a4eb-1dc34dea1ab1",
-        title: "Home Decor",
-        handle: "decor",
-      },
-    ],
-  },
-  adminshopify: {
-    legalContent: {
-      nodes: [
-        {
-          fields: [
-            {
-              key: "return_and_refund_policy",
-              definition: {
-                name: "Return and Refund Policy",
-              },
-            },
-            {
-              key: "hand_made_policy",
-              definition: {
-                name: "Hand Made Policy",
-              },
-            },
-            {
-              key: "shipping_policy",
-              definition: {
-                name: "Shipping Policy",
-              },
-            },
-            {
-              key: "privacy_policy",
-              definition: {
-                name: "Privacy Policy",
-              },
-            },
-            {
-              key: "terms_of_service",
-              definition: {
-                name: "Terms of Service",
-              },
-            },
-          ],
-        },
-      ],
-    },
-  },
-};
+const useStaticQuery = jest.spyOn(Gatsby, `useStaticQuery`);
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -106,8 +14,99 @@ afterEach(() => {
 });
 
 describe("Layout", () => {
+  const mockUseStaticQuery = {
+    site: {
+      siteMetadata: {
+        title: "Site Title",
+      },
+    },
+    allShopifyCollection: {
+      nodes: [
+        {
+          id: "4179b182-b572-5609-a8a2-05083671479a",
+          title: "Murals & Sign Writing",
+          handle: "murals",
+        },
+        {
+          id: "f824e7dd-2243-5286-a2aa-162cbc0f42cd",
+          title: "Stickers",
+          handle: "stickers",
+        },
+        {
+          id: "a1f141d4-942b-51bc-9ab2-a2f4a48d1755",
+          title: "Wearable Art",
+          handle: "wearable-art",
+        },
+        {
+          id: "1f50e1f9-d3a0-54cc-b961-652870d93340",
+          title: "Resin & Pigment Art",
+          handle: "resin-and-pigment-art",
+        },
+        {
+          id: "62470692-0598-57cc-bc0a-1ea60791c995",
+          title: "Commissions",
+          handle: "commissions",
+        },
+        {
+          id: "bd4e6e5b-a663-5e00-becb-b4a63d4c7ec6",
+          title: "Prints",
+          handle: "prints",
+        },
+        {
+          id: "ab94a31f-8fc3-5e3c-b0cf-5a16c873d647",
+          title: "Original Paintings",
+          handle: "originals",
+        },
+        {
+          id: "384d31c2-36b3-5bc7-a4eb-1dc34dea1ab1",
+          title: "Home Decor",
+          handle: "decor",
+        },
+      ],
+    },
+    adminshopify: {
+      legalContent: {
+        nodes: [
+          {
+            fields: [
+              {
+                key: "return_and_refund_policy",
+                definition: {
+                  name: "Return and Refund Policy",
+                },
+              },
+              {
+                key: "hand_made_policy",
+                definition: {
+                  name: "Hand Made Policy",
+                },
+              },
+              {
+                key: "shipping_policy",
+                definition: {
+                  name: "Shipping Policy",
+                },
+              },
+              {
+                key: "privacy_policy",
+                definition: {
+                  name: "Privacy Policy",
+                },
+              },
+              {
+                key: "terms_of_service",
+                definition: {
+                  name: "Terms of Service",
+                },
+              },
+            ],
+          },
+        ],
+      },
+    },
+  };
+
   it("renders mobile layout correctly", async () => {
-    const useStaticQuery = jest.spyOn(Gatsby, `useStaticQuery`);
     useStaticQuery.mockImplementation(() => mockUseStaticQuery);
     Object.defineProperty(window, "matchMedia", {
       value: jest.fn(() => ({
@@ -133,7 +132,6 @@ describe("Layout", () => {
   });
 
   it("loads desktop layout", async () => {
-    const useStaticQuery = jest.spyOn(Gatsby, `useStaticQuery`);
     useStaticQuery.mockImplementation(() => mockUseStaticQuery);
     Object.defineProperty(window, "matchMedia", {
       value: jest.fn(() => ({
