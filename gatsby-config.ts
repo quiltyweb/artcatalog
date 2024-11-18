b731c63ce972d7403dde2482524ebcd5021ec22a
melissa gattoni <meligatt@gmail.com>
2024-11-18 15:05:34 +1100

    update store url option with admin url

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -35,10 +35,10 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-source-shopify`,
       options: {
-        storeUrl: process.env.GATSBY_SHOPIFY_STOREFRONT_URL,
+        storeUrl: process.env.GATSBY_SHOPIFY_ADMIN_URL,
         password: process.env.GATSBY_SHOPIFY_ADMIN_PASSWORD,
         salesChannel: process.env.SHOPIFY_APP_ID,
-        shopifyConnections: ["orders", "collections", "locations"],
+        shopifyConnections: ["collections", "locations"],
       },
     },
     {
