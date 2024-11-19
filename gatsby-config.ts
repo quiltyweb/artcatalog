a12a143572df6238c8d7d3093ea7f0a14321e5af
melissa gattoni <meligatt@gmail.com>
2024-11-19 15:15:15 +1100

    update env vars

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,9 +5,6 @@ dotenv.config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
-// eslint-disable-next-line no-console
-console.log(process.env.NODE_ENV, process.env);
-
 const config: GatsbyConfig = {
   siteMetadata: {
     title: "Brushella",
@@ -40,7 +37,7 @@ const config: GatsbyConfig = {
       options: {
         storeUrl: process.env.GATSBY_SHOPIFY_ADMIN_URL,
         password: process.env.GATSBY_SHOPIFY_ADMIN_PASSWORD,
-        salesChannel: process.env.SHOPIFY_APP_ID,
+        salesChannel: process.env.GATSBY_SHOPIFY_APP_ID,
         shopifyConnections: ["collections"],
       },
     },
