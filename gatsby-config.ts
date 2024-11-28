df437451dc8163c2b7b32d8a4d3a242ab0451866
melissa gattoni <meligatt@gmail.com>
2024-11-28 22:34:16 +1100

    remove no secret strings from api auth

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -37,7 +37,7 @@ const config: GatsbyConfig = {
       options: {
         storeUrl: "brushella-dev.myshopify.com",
         password: process.env.GATSBY_SHOPIFY_ADMIN_PASSWORD,
-        salesChannel: process.env.GATSBY_SHOPIFY_APP_ID,
+        salesChannel: "brushella_app",
         shopifyConnections: ["collections"],
       },
     },
