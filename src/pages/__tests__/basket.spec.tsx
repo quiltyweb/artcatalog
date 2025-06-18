7aaa65a29706bbc17d1f1da58719126f260ab04c
melissa gattoni <meligatt@gmail.com>
2025-06-18 10:43:44 +1000

    update unit test with is loading false as default value

--- src/pages/__tests__/basket.spec.tsx
+++ src/pages/__tests__/basket.spec.tsx
@@ -14,6 +14,7 @@ describe("BasketPage", () => {
   beforeEach(() => {
     jest.clearAllMocks();
     fetchMock.resetMocks();
+    useIsCartLoading.mockImplementation(() => false);
   });
 
   afterEach(() => {
