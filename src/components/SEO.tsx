3420075587801198734026820c24906619e6a8ea
melissa gattoni <meligatt@gmail.com>
2026-04-24 12:35:53 +1000

    add preconnect and remove unused slick package

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -34,17 +34,7 @@ const SEO: React.FunctionComponent<SEOProps> = ({
       <meta property="og:image" content={image} />
       <meta property="og:site_name" content={siteTitle} />
       {canonical && <meta property="og:url" content={canonical} />}
-      <link
-        rel="stylesheet"
-        type="text/css"
-        charSet="UTF-8"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-      />
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-      />
+      <link rel="preconnect" href="https://cdn.shopify.com" />
       {children}
     </>
   );
