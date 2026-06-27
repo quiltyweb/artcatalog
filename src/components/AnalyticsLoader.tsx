5fdd9b720f46c598d6684cd775d446711cf0deac
melissa gattoni <meligatt@gmail.com>
2026-06-27 16:55:28 +1000

    install pixel

--- src/components/AnalyticsLoader.tsx
+++ src/components/AnalyticsLoader.tsx
@@ -5,6 +5,8 @@ declare global {
   interface Window {
     dataLayer: unknown[];
     gtag: (...args: unknown[]) => void;
+    fbq: (...args: unknown[]) => void;
+    _fbq: unknown;
   }
 }
 
@@ -32,6 +34,38 @@ const AnalyticsLoader: React.FunctionComponent = (): null => {
     gtag("config", measurementId, { send_page_view: true });
   }, [consent]);
 
+  React.useEffect(() => {
+    const pixelId = process.env.GATSBY_META_PIXEL_ID;
+    if (consent !== "granted" || !pixelId) return;
+    if (document.getElementById("meta-pixel-script")) return;
+    window.fbq = function (...args: unknown[]) {
+      const fbq = window.fbq as unknown as {
+        callMethod?: (...a: unknown[]) => void;
+        queue: unknown[][];
+      };
+      fbq.callMethod
+        ? fbq.callMethod(...args)
+        : (fbq.queue = fbq.queue || []).push(args);
+    };
+    if (!window._fbq) window._fbq = window.fbq;
+    const meta = window.fbq as unknown as {
+      push: unknown;
+      loaded: boolean;
+      version: string;
+      queue: unknown[][];
+    };
+    meta.push = window.fbq as unknown as unknown[];
+    meta.loaded = true;
+    meta.version = "2.0";
+    meta.queue = [];
+    const script = document.createElement("script");
+    script.id = "meta-pixel-script";
+    script.async = true;
+    script.src = "https://connect.facebook.net/en_US/fbevents.js";
+    document.head.appendChild(script);
+    window.fbq("init", pixelId);
+    window.fbq("track", "PageView");
+  }, [consent]);
   return null;
 };
 
